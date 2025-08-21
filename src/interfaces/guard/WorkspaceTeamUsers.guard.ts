4c8a4a85b6d15c02be92e159fe75b2be72161534
suhyun751207 <23s607h0659@gmail.com>
2025-08-21 22:18:31 +0900

    chrome

--- src/interfaces/guard/WorkspaceTeamUsers.guard.ts
+++ src/interfaces/guard/WorkspaceTeamUsers.guard.ts
@@ -1,7 +1,7 @@
 import { createTypeGuard } from "type-wizard";
 import { UserCreate } from "../WorkspaceTeamUsers.ts";
 
-export const isUserCreate=createTypeGuard<UserCreate>({
+export const isWorkspaceTeamUserCreate=createTypeGuard<UserCreate>({
     memberId: {type: "number"},
     teamId: {type: "number"},
     role: {type: "string"}
