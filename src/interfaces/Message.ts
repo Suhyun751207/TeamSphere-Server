d0142c49ee2fd366b6f522731882ecc5b7fde958
suhyun751207 <23s607h0659@gmail.com>
2025-08-26 13:02:35 +0900

    feat

--- src/interfaces/Message.ts
+++ src/interfaces/Message.ts
@@ -1,6 +1,16 @@
-import { MessageType } from "@services/ENUM/chat_type_enum.ts";
+import { ChatType, MessageType } from "@services/ENUM/chat_type_enum.ts";
 
+export const messagesKeys = ['id', 'chatType', 'chatId', 'name', 'createdAt', 'updatedAt'] as const;
 export const messageKeys = ['id', 'userId', 'content', 'messageType', 'replyToId', 'createdAt', 'updatedAt', 'isDeleted', 'isEdited'] as const;
+
+export interface Messages {
+  id: number;
+  chatType: ChatType;
+  chatId: number;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 export interface Message {
   id: number;
@@ -14,7 +24,11 @@ export interface Message {
   isEdited: number;
 }
 
+export type MessagesAutoSetKeys = "id" | "createdAt" | "updatedAt";
 export type MessageAutoSetKeys = "id" | "createdAt" | "updatedAt";
+
+export interface MessagesCreate extends Omit<Messages, MessagesAutoSetKeys> {}
+export interface MessagesUpdate extends Partial<Omit<Messages, MessagesAutoSetKeys | "chatType">> {}
 
 export interface MessageCreate extends Omit<Message, MessageAutoSetKeys> {}
 export interface MessageUpdate extends Partial<Omit<Message, MessageAutoSetKeys | "userId">> {}
