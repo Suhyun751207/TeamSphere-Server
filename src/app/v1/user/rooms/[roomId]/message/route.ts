f849515f62501ac41ef79673a134cb6c2f526f43
suhyun751207 <23s607h0659@gmail.com>
2025-08-29 16:08:35 +0900

    feat

--- src/app/v1/user/rooms/[roomId]/message/route.ts
+++ src/app/v1/user/rooms/[roomId]/message/route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import catchAsyncErrors from "@utils/catchAsyncErrors.ts";
 import { authenticateToken } from "@middleware/auth.ts";
 import messageService from "@services/message";
-import { isMessageCreate } from "@interfaces/guard/Message.guard";
+import { isMessageCreate, isMessageUpdate } from "@interfaces/guard/Message.guard";
 
 const messageRouter = Router({ mergeParams: true });
 
@@ -17,6 +17,18 @@ messageRouter.post('/', authenticateToken, catchAsyncErrors(async (req, res) => 
     if (!isMessageCreate(data)) return res.status(400).json({ message: isMessageCreate.message(data) });
     const message = await messageService.create(data);
     return res.status(201).json(message);
+}));
+
+// 메시지 id를 받아 메시지 데이터를 read하고 read data 속 userId와 authenticateToken에서 오는 userId 일치하면 수정 진행
+messageRouter.patch('/:messageId', authenticateToken, catchAsyncErrors(async (req, res) => {
+    const messageId = req.params.messageId;
+    const message = await messageService.read(Number(messageId));
+    if (!message) return res.status(404).json({ message: "Message not found" });
+    if (message.userId !== Number(req.user?.userId)) return res.status(401).json({ message: "Unauthorized" });
+    const data = { roomId: Number(req.params.roomId), userId: Number(req.user?.userId), type: "TEXT", imagePath: req.body.imagePath || null, content: req.body.content, isEdited: true, isValid: true };
+    if (!isMessageUpdate(data)) return res.status(400).json({ message: isMessageUpdate.message(data) });
+    const updatedMessage = await messageService.update(Number(messageId), data);
+    return res.status(200).json(updatedMessage);
 }));
 
 export default messageRouter;
