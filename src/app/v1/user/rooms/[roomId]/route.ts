35c4909292bad1ca678e11cead7ccc430aad8b54
suhyun751207 <23s607h0659@gmail.com>
2025-08-29 21:22:28 +0900

    feat

--- src/app/v1/user/rooms/[roomId]/route.ts
+++ src/app/v1/user/rooms/[roomId]/route.ts
@@ -5,9 +5,17 @@ import roomsService from "@services/Rooms.ts";
 import { isRoomsCreate } from "@interfaces/guard/Rooms.guard";
 import roomsUserIdRouter from "./[userId]/route.ts";
 import messageRouter from "./message/route.ts";
+import messageService from "@services/message.ts";
 
 const roomIdRouter = Router({ mergeParams: true });
 roomIdRouter.use('/:userId', roomsUserIdRouter);
 roomIdRouter.use('/message', messageRouter);
+
+
+roomIdRouter.get('/', authenticateToken, catchAsyncErrors(async (req, res) => {
+    const roomId = req.params.roomId;
+    const messages = await messageService.read(Number(roomId));
+    return res.status(200).json(messages);
+}));
 
 export default roomIdRouter;
