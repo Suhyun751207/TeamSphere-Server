115673b756bdbecba991a8bf7db5896e487da681
suhyun751207 <23s607h0659@gmail.com>
2025-08-29 21:22:47 +0900

    fix

--- src/app/v1/user/rooms/[roomId]/message/route.ts
+++ src/app/v1/user/rooms/[roomId]/message/route.ts
@@ -6,12 +6,6 @@ import { isMessageCreate, isMessageUpdate } from "@interfaces/guard/Message.guar
 
 const messageRouter = Router({ mergeParams: true });
 
-messageRouter.get('/', authenticateToken, catchAsyncErrors(async (req, res) => {
-    const roomId = req.params.roomId;
-    const messages = await messageService.read(Number(roomId));
-    return res.status(200).json(messages);
-}));
-
 messageRouter.post('/', authenticateToken, catchAsyncErrors(async (req, res) => {
     const data = { roomId: Number(req.params.roomId), userId: Number(req.user?.userId), type: "TEXT", content: req.body.content };
     if (!isMessageCreate(data)) return res.status(400).json({ message: isMessageCreate.message(data) });
