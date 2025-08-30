6015eacef087d4e12b2fa654bbed0e201e3803d0
suhyun751207 <23s607h0659@gmail.com>
2025-08-30 21:49:23 +0900

    docs

--- src/app/v1/user/rooms/[roomId]/route.ts
+++ src/app/v1/user/rooms/[roomId]/route.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import catchAsyncErrors from "@utils/catchAsyncErrors.ts";
 import { authenticateToken } from "@middleware/auth.ts";
-import roomsService from "@services/Rooms.ts";
-import { isRoomsCreate } from "@interfaces/guard/Rooms.guard";
 import roomsUserIdRouter from "./[userId]/route.ts";
 import messageRouter from "./message/route.ts";
 import messageService from "@services/message.ts";
