1137b23c5941d1baf0aa04a6de003224a5429169
suhyun751207 <23s607h0659@gmail.com>
2025-09-14 23:50:17 +0900

    fix

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,6 +2,7 @@
 import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS!); // JSON 키 import
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
