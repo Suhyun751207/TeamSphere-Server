69941b36999969f52205bc3431c730d6ac79935f
suhyun751207 <23s607h0659@gmail.com>
2025-09-14 23:54:23 +0900

    fix

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,12 +1,21 @@
 // src/firebase.ts
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS!); // JSON 키 import
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+} as unknown as admin.ServiceAccount;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "teamsphere-ae765.firebasestorage.app", // Firebase Storage 버킷 이름
+  storageBucket: "teamsphere-ae765.appspot.com", // Firebase Storage 버킷 이름
 });
 
 const bucket = admin.storage().bucket();
